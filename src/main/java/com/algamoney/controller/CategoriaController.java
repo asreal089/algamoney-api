9919c029c0e8a8fdbb177fc44ef08b4ae9aca24a
asreal089 <lexx_89@hotmail.com>
2020-11-23 22:49:41 -0300

    Add event listner e publisher

--- src/main/java/com/algamoney/controller/CategoriaController.java
+++ src/main/java/com/algamoney/controller/CategoriaController.java
@@ -1,6 +1,5 @@
 package com.algamoney.controller;
 
-import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
@@ -8,6 +7,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,8 +17,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.algamoney.event.RecursoCriadoEvent;
 import com.algamoney.model.Categoria;
 import com.algamoney.repository.CategoriaRepository;
 
@@ -29,6 +29,9 @@ public class CategoriaController {
 	@Autowired
 	private CategoriaRepository categoriaRepository;
 	
+	@Autowired
+	private ApplicationEventPublisher publisher;
+	
 	@GetMapping
 	public ResponseEntity<?> listaCategorias(){
 		List<Categoria> categorias =  categoriaRepository.findAll();
@@ -37,11 +40,10 @@ public class CategoriaController {
 	
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
-	public void criarCategoria(@Valid @RequestBody Categoria categoria, HttpServletResponse response) {
+	public ResponseEntity<Categoria> criarCategoria(@Valid @RequestBody Categoria categoria, HttpServletResponse response) {
 		Categoria categoriaSalva = categoriaRepository.save(categoria);
-		URI uri =ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{codigo}").buildAndExpand(categoriaSalva.getCodigo()).toUri();
-		response.setHeader("Location", uri.toASCIIString());
-		
+		publisher.publishEvent(new RecursoCriadoEvent(this, response, categoriaSalva.getCodigo()));
+		return ResponseEntity.status(HttpStatus.CREATED).body(categoriaSalva);
 	}
 	
 	@GetMapping("/{codigo}")
--- src/main/java/com/algamoney/controller/PessoaController.java
+++ src/main/java/com/algamoney/controller/PessoaController.java
@@ -1,6 +1,5 @@
 package com.algamoney.controller;
 
-import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
@@ -8,6 +7,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,8 +17,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.algamoney.event.RecursoCriadoEvent;
 import com.algamoney.model.Pessoa;
 import com.algamoney.repository.PessoaRepository;
 
@@ -26,7 +26,9 @@ import com.algamoney.repository.PessoaRepository;
 @RequestMapping("/pessoas")
 public class PessoaController {
 	
-
+	@Autowired
+	private ApplicationEventPublisher publisher;
+	
 	@Autowired 
 	private PessoaRepository pessoaRepository;
 	
@@ -44,10 +46,9 @@ public class PessoaController {
 	
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
-	public void criarCategoria(@Valid @RequestBody Pessoa pessoa, HttpServletResponse response) {
+	public ResponseEntity<Pessoa> criarCategoria(@Valid @RequestBody Pessoa pessoa, HttpServletResponse response) {
 		Pessoa pessoaSalva = pessoaRepository.save(pessoa);
-		URI uri =ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{codigo}").buildAndExpand(pessoaSalva.getCodigo()).toUri();
-		response.setHeader("Location", uri.toASCIIString());
-		
+		publisher.publishEvent(new RecursoCriadoEvent(this, response, pessoaSalva.getCodigo()));
+		return ResponseEntity.status(HttpStatus.CREATED).body(pessoaSalva);
 	}
 }
